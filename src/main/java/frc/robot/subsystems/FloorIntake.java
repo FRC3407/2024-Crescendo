eb97640f6b2b503ed0ae044142f4cba441a0dd2a
Keith Rieck <KeithRieck@gmail.com>
2024-03-24 12:49:11 -0500

    Update RevRobotics library and imports

--- src/main/java/frc/robot/subsystems/FloorIntake.java
+++ src/main/java/frc/robot/subsystems/FloorIntake.java
@@ -1,12 +1,9 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.AnalogTrigger;
-import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
