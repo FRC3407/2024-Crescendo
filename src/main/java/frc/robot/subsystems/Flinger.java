da514836906450aa96cb560f12f2a06f136bb4dd
Sam Richter <hoodie888@outlook.com>
2024-04-05 14:16:49 -0500

    theoretical NT + offline logging for flinger and intake

--- src/main/java/frc/robot/subsystems/Flinger.java
+++ src/main/java/frc/robot/subsystems/Flinger.java
@@ -4,6 +4,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -25,11 +27,7 @@ public class Flinger extends SubsystemBase {
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Flinger Encoder Velocity 1", getRPM_1());
-        SmartDashboard.putNumber("Flinger Target Velocity 1", getTargetRPM());
         
-        SmartDashboard.putNumber("Flinger Encoder Velocity 2", getRPM_2());
-        SmartDashboard.putNumber("Flinger Target Velocity 2", getTargetRPM());
     }
 
     @Override
@@ -61,6 +59,19 @@ public class Flinger extends SubsystemBase {
     public double getRPM_2()
     {
         return flingerMotor_2.getEncoder().getVelocity();
+    }
+
+
+    @Override
+    public void initSendable(SendableBuilder b) {
+        b.addDoubleProperty("Encoder RPM 1", this::getRPM_1, null);
+        b.addDoubleProperty("Encoder RPM 2", this::getRPM_2, null);
+        b.addDoubleProperty("Target RPM 1", this::getTargetRPM, null);
+        b.addDoubleProperty("Target RPM 2", this::getTargetRPM, null);
+        b.addDoubleProperty("Output Current 1", this.flingerMotor_1::getOutputCurrent, null);
+        b.addDoubleProperty("Output Current 2", this.flingerMotor_2::getOutputCurrent, null);
+        b.addDoubleProperty("Bus Voltage 1", this.flingerMotor_1::getBusVoltage, null);
+        b.addDoubleProperty("Bus Voltage 2", this.flingerMotor_2::getBusVoltage, null);
     }
 
 }
